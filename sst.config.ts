ef27a2e81190c6e9e191dd4d54bfdaa11c74676b
Marty Penner <martypenner@users.noreply.github.com>
2024-08-05 19:43:40 -0400

    feat: automate deployment using sst

---
+++ sst.config.ts
@@ -0,0 +1,48 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+import path from "node:path";
+
+export default $config({
+  app(input) {
+    return {
+      name: "tclip",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      home: "cloudflare",
+      providers: {
+        tailscale: true,
+        docker: true,
+        cloudflare: true,
+      },
+    };
+  },
+  async run() {
+    const tailnetKey = new tailscale.TailnetKey("tclipTailnetKey", {
+      reusable: true,
+      ephemeral: true,
+      preauthorized: true,
+      expiry: 60 * 60 * 24 * 7,
+    });
+
+    const appNetwork = new docker.Network("tclip-network", {
+      name: "tclip-network",
+      driver: "bridge",
+    });
+
+    const appContainer = new docker.Container("tclip", {
+      name: "tclip",
+      image: "ghcr.io/tailscale-dev/tclip:latest",
+      hostname: "tclip",
+      networksAdvanced: [{ name: appNetwork.name }],
+      restart: "unless-stopped",
+      mounts: [
+        {
+          target: "/data",
+          source: path.join(process.cwd(), "./data"),
+          type: "bind",
+          readOnly: false,
+        },
+      ],
+      envs: [$interpolate`TS_AUTHKEY=${tailnetKey.key}`],
+    });
+  },
+});
